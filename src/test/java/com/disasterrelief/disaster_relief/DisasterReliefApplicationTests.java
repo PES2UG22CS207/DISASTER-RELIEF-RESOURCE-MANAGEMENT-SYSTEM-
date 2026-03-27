4282b8a8a4eb2743b67d52b8acde7ea06c6e5afc
PES2UG23CS065 <pes2ug23cs065@pesu.pes.edu>
2026-03-27 15:41:04 +0530

    Configured database connection

---
+++ src/test/java/com/disasterrelief/disaster_relief/DisasterReliefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disasterrelief.disaster_relief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterReliefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
